fe28c452a52d9c088e1960098ee2019f1eb8522e
AMIT <kumarsamit335@gmail.com>
2021-05-30 23:31:42 -0700

    js

---
+++ js/paralax.js
@@ -0,0 +1,55 @@
+var MenuItems = document.getElementById("MenuItems");
+function menutoggle(){
+    if(MenuItems.style.maxWidth=="0px")
+        {
+            MenuItems.style.maxWidth="90vw";
+            MenuItems.style.opacity="1";
+        }
+    else
+        {
+            MenuItems.style.maxWidth="0px";
+            MenuItems.style.opacity="0";
+
+        }          
+}
+
+    
+
+
+
+    const parallax = document.getElementById("parallax");
+
+    window.addEventListener("scroll", function()
+    {
+        let offset = window.pageYOffset;
+        parallax.style.backgroundPositionY = offset * .9 + "px";
+    })
+
+
+    const parallax_1 = document.getElementById("parallax_1");
+    window.addEventListener("scroll", function()
+    {
+        let offset = window.pageYOffset;
+        parallax_1.style.backgroundPositionY = offset * 1.1 + "px";
+    })
+
+    const parallax_2 = document.getElementById("parallax_2");
+    window.addEventListener("scroll", function()
+    {
+        let offset = window.pageYOffset;
+        parallax_2.style.backgroundPositionY = offset * .98 + "px";
+    })
+
+    const parallax_3 = document.getElementById("parallax_3");
+    window.addEventListener("scroll", function()
+    {
+        let offset = window.pageYOffset;
+        parallax_3.style.backgroundPositionY = offset * 1.1 + "px";
+    })
+
+    const parallax_4 = document.getElementById("parallax_4");
+    window.addEventListener("scroll", function()
+    {
+        let offset = window.pageYOffset;
+        parallax_4.style.backgroundPositionY = offset * .9 + "px";
+    })
